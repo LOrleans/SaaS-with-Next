1b630310ddb1278f7e0d9df01829612aa10ef92d
Lucas_Orleans <lucasorleans.pro@gmail.com>
2026-01-06 14:00:56 -0300

    Finaliza Landing Page

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,17 @@
+import Header from "../components/landing-page/Header";
+import Hero from "../components/landing-page/Hero";
+import VideoExplanation from "../components/landing-page/VideoExplanation";
+import Pricing from "../components/landing-page/Pricing";
+import FAQ from "../components/landing-page/FAQ";
+
+export default function Home() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Header />
+      <Hero />
+      <VideoExplanation />
+      <Pricing />
+      <FAQ />
+    </div>
+  );
+}

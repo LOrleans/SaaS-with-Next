615608dcbabf55f81d9ed865cf2226b368a4175f
Lucas_Orleans <lucasorleans.pro@gmail.com>
2026-01-06 14:25:08 -0300

    Finaliza frontend

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -8,7 +8,7 @@ export default function Button({
   return (
     <button 
       className={cn(
-        "p-3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70",
+        "p-3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 cursor-pointer disabled:opacity-70",
         variant === 'primary' && 'bg-accent-purple',
         variant === 'secondary' && 'bg-background-tertiary',
         variant === 'ghost' && 'border-border-primary',
